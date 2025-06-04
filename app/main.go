ddd70ae3c19de166a72ca89c2b998049041e8795
Hruday Chowdary Gurijala <gurijalahruday@gmail.com>
2025-06-04 22:36:13 +0530

    reading headers

--- app/main.go
+++ app/main.go
@@ -28,6 +28,15 @@ func connHandler(conn net.Conn) {
 	lines := strings.Split(request, "\r\n")
 	if len(lines) > 0 {
 		requestLine := lines[0]
+		var userAgentContent string
+		for i := 1; i < len(lines); i++ {
+			if strings.Contains(lines[i], "User-Agent:") {
+				userAgentLine := strings.Split(lines[i], ": ")
+				userAgentContent = userAgentLine[1]
+				break
+			}
+		}
+
 		parts := strings.Split(requestLine, " ")
 		if len(parts) >= 2 {
 			method := parts[0]
@@ -41,6 +50,11 @@ func connHandler(conn net.Conn) {
 				content := strings.TrimPrefix(url, "/echo/")
 				contentLength := strconv.Itoa(len(content))
 				response := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + contentLength + "\r\n\r\n" + content
+				conn.Write([]byte(response))
+				return
+			} else if url == "/user-agent" {
+				contentLength := strconv.Itoa(len(userAgentContent))
+				response := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + contentLength + "\r\n\r\n" + userAgentContent
 				conn.Write([]byte(response))
 				return
 			}
